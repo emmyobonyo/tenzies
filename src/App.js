5edafb0ccccbc0023ee78369f1fa08fcd0538bff
emmyobonyo@gmail.com <emmyobonyo@gmail.com>
2022-03-18 13:44:09 +0300

    Change the color of the held dice

--- src/App.js
+++ src/App.js
@@ -1,18 +1,27 @@
 import React from 'react'
 import Dice from './components/Dice';
+import {nanoid} from 'nanoid';
 import './App.css';
 
 function App() {
   const [dice, setDice] = React.useState(allNewDice()); // using function to initialise state
   function allNewDice() {
     const newDice = []
-    for(let i = 0; i<10; i++){
-      newDice.push(Math.ceil(Math.random() * 6)) //it's a function so it ought to run immediately
+    for (let i = 0; i < 10; i++) {
+        newDice.push({
+            id: nanoid(),
+            value: Math.ceil(Math.random() * 6),
+            isHeld: true
+        })
     }
     return newDice
   }
 
-  const diceElements = dice.map(die => <Dice value={die}/>)
+  function rollDice(){
+    setDice(allNewDice())
+  }
+
+  const diceElements = dice.map(die => <Dice key={die.id} value={die.value} held={die.isHeld}/>)
 
   return (
     <div className="App">
@@ -20,6 +29,7 @@ function App() {
           <div className="dice-container">
             {diceElements}
           </div>
+          <button className="roll-dice" onClick={rollDice}>Roll</button>
         </main>
     </div>
   );
--- src/components/Dice.js
+++ src/components/Dice.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 function Dice(props) {
   return (
-    <div className="die-face">
+    <div className={ props.held ? "die-face-held": "die-face"}>
       <h2 className="die-num">{props.value}</h2>
     </div>
   )
