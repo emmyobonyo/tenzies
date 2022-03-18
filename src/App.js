d613959af0eff9326ca8079a01282206d5d3d37f
emmyobonyo@gmail.com <emmyobonyo@gmail.com>
2022-03-18 12:34:40 +0300

    Update dice to recieve a random number

--- src/App.js
+++ src/App.js
@@ -1,9 +1,26 @@
+import React from 'react'
+import Dice from './components/Dice';
 import './App.css';
 
 function App() {
+  const [dice, setDice] = React.useState(allNewDice()); // using function to initialise state
+  function allNewDice() {
+    const newDice = []
+    for(let i = 0; i<10; i++){
+      newDice.push(Math.ceil(Math.random() * 6)) //it's a function so it ought to run immediately
+    }
+    return newDice
+  }
+
+  const diceElements = dice.map(die => <Dice value={die}/>)
+
   return (
     <div className="App">
-        <main></main>
+        <main>
+          <div className="dice-container">
+            {diceElements}
+          </div>
+        </main>
     </div>
   );
 }
